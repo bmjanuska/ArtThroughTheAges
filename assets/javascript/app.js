863c361aaec8516691fd29d101fdfa0cb061dbc8
Bernadette Januska <bmjanuska@gmail.com>
2018-07-25 17:00:37 -0400

    Sorting logic

---
+++ assets/javascript/app.js
@@ -0,0 +1,103 @@
+// What to do 
+// Have a start screen and a button to initialize the timer and set of questions
+
+// Timer starts and calls the following 
+// Make an array for questions
+// Variable for wins 
+// Variable for loss
+// Variable for incomplete
+// Variable for timer
+
+// Call length of questions 
+// Use if and else to determine correct or incorrect value ( or even if it is unanswered) 
+
+// Have a screen that prints out the final score and a message! 
+
+
+
+
+//======== Variables =========
+	
+	//======== Scores =========
+	win = 0,
+	loss = 0; 
+	unanswer = 0;
+	
+	//======== Timers =========
+
+	//======== Questions =========
+
+	var artQuestions = [
+	{
+		question: "This vegetarian and animal rights activist painted the Mona lisa.",
+		answers: {
+			a: "Pablo Ruiz Picasso",  
+			b: "Leonardo DiCaprio",
+			c: "Leonardo Da Vinci",
+			d: "Andrea Salai"
+		},
+		correctAns: "c"
+	},
+
+	{
+		question: "“I saw the angel in the marble and carved until I set him free.”  Was a quote from which sculptor? ",
+		answers: {
+			a: "Michelangelo",  
+			b: "Gian Lorenzo Bernini",
+			c: "Antonio Canova",
+			d: "Phidias"
+		},
+		correctAns: "a"
+	},
+
+	{
+		question: "Van Gogh was a(n)  _____ painter.",
+		answers: {
+			a: "Abstract Expressionist",  
+			b: "Early Baroque",
+			c: "Mural",
+			d: "Post-Impressionist"
+		},
+		correctAns: "d"
+	},
+
+	{
+		question: "This fashionable icon is known for Pop Art depiction of a Campbell’s Soup can actually comes in a set of 32 silkscreened canvases, each representing the 32 separate soup varieties that the company sold at the time.",
+		answers: {
+			a: "Willem De Cooning",  
+			b: "Robert Rauschenberg",
+			c: "Marcel Duchamp",
+			d: " Andy Warhol"
+		},
+		correctAns: "d"
+	},
+
+	{
+		question: " ----",
+		answers: {
+			a: "Willem De Cooning",  
+			b: "Robert Rauschenberg",
+			c: "Marcel Duchamp",
+			d: " Andy Warhol"
+		},
+		correctAns: "d"
+	},
+
+	{
+		question: " ----",
+		answers: {
+			a: "Willem De Cooning",  
+			b: "Robert Rauschenberg",
+			c: "Marcel Duchamp",
+			d: " Andy Warhol"
+		},
+		correctAns: "d"
+	},
+
+	
+	]
+
+//======== Functions =========
+
+
+//======== Variables =========
